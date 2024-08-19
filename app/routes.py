0e3727afc6b16cb329a78bb931eff5af3c9c1dc6
Andrea Temin <AndreaTemin@gmail.com>
2024-08-19 09:23:20 +0300

    separating logic with services

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.models import WeightUpdate
-from app.data import data
+
 # from app.models import WeightUpdate
 from app.services import (
     get_weights_service,
@@ -13,18 +13,17 @@ router = APIRouter()
 
 @router.get("/client/{client_id}/weights")
 def get_weights(client_id: str):
-    return get_weights_service()
-
+    return get_weights_service(client_id)
 
 @router.put("/client/{client_id}/weights/source/{source_name}")
 def update_source_weight(client_id: str, source_name: str, weight_update: WeightUpdate):
     print("something in the way it moves")
-    return update_source_weight_service(source_name, weight_update)
+    return update_source_weight_service(client_id, source_name, weight_update)
 
 @router.put("/client/{client_id}/weights/source/{source_name}/category/{category_name}")
 def update_category_weight(client_id: str, source_name: str, category_name: str, weight_update: WeightUpdate):
-    return update_category_weight_service(source_name, category_name, weight_update)
+    return update_category_weight_service(client_id, source_name, category_name, weight_update)
 
 @router.put("/client/{client_id}/weights/source/{source_name}/category/{category_name}/campaign/{campaign_name}")
 def update_campaign_weight(client_id: str, source_name: str, category_name: str, campaign_name: str, weight_update: WeightUpdate):
-    return update_campaign_weight_service(source_name, category_name, campaign_name, weight_update)
+    return update_campaign_weight_service(client_id, source_name, category_name, campaign_name, weight_update)
--- app/services.py
+++ app/services.py
@@ -1,60 +1,66 @@
-# app/services.py
 
 from fastapi import HTTPException
-from app.costum_error import WeightOutOfRangeException
-from app.data import data
 from app.models import Source, Category, Campaign, WeightUpdate
+from app.data import get_clients_data
 
-def get_weights_service():
-    return data
+def get_weights_service(client_id: str):
+    return get_clients_data(client_id)
 
-def __validate_weight_update(weight):
-    if isinstance(WeightUpdate, weight):
-        raise HTTPException(status_code=400, detail="Weight is wrong")
-    if not 0 <= weight <= 100:
-        raise WeightOutOfRangeException
-
-def update_source_weight_service(source_name: str, weight_update: WeightUpdate):
+def update_source_weight_service(client_id: str, source_name: str, weight_update: WeightUpdate):
+    data = get_clients_data(client_id)
+    
     if source_name not in data:
         raise HTTPException(status_code=404, detail="Source not found")
 
-
-    # - The total weight of all sources must be a non-negative number
-    # and individual updates should not cause the weight to go below zero, 
-    # but this cannot happen if the next point is enforced 
-    
-    # - The weight (`w`) for any entity should be between 0 and 100.
-    __validate_weight_update(weight_update)   
+    total_weight = sum(s["w"] for s in data.values()) - data[source_name]["w"] + weight_update.w
+    if total_weight > 100:
+        raise HTTPException(400, "Weight of a source cannot exceed 100")
 
     data[source_name]["w"] = weight_update.w
     return data[source_name]
 
-def update_category_weight_service(source_name: str, category_name: str, weight_update: WeightUpdate):
-    if source_name not in data or "categories" not in data[source_name] or category_name not in data[source_name]["categories"]:
+
+def update_category_weight_service(client_id: str, source_name: str, category_name: str, weight_update: WeightUpdate):
+    data = get_clients_data(client_id)
+    
+    if source_name not in data \
+        or "categories" not in data[source_name] \
+            or category_name not in data[source_name]["categories"]:
         raise HTTPException(status_code=404, detail="Category not found")
-
-    source = Source(**data[source_name])
-    category = Category(**source.categories[category_name])
-
-    total_category_weight = sum(c.w for c in source.categories.values()) - category.w + weight_update.w
+    
+    current_category = data[source_name]["categories"][category_name]
+    
+    total_category_weight = \
+        sum(category["w"] for category in data[source_name]["categories"].values()) \
+        - current_category["w"] + weight_update.w
+    
     if total_category_weight > 100:
-        raise HTTPException(status_code=400, detail="Total category weight exceeds 100 within the source")
+        raise HTTPException(status_code=400, detail=f"Total category weight exceeds 100 within the source: {source_name}"
+        )
+    
+    current_category["w"] = weight_update.w
+    return current_category
 
-    category.w = weight_update.w
-    data[source_name]["categories"][category_name] = category.model_dump()
-    return data[source_name]["categories"][category_name]
 
-def update_campaign_weight_service(source_name: str, category_name: str, campaign_name: str, weight_update: WeightUpdate):
-    if source_name not in data or "categories" not in data[source_name] or category_name not in data[source_name]["categories"] or "campaigns" not in data[source_name]["categories"][category_name] or campaign_name not in data[source_name]["categories"][category_name]["campaigns"]:
+
+def update_campaign_weight_service(client_id: str, source_name: str, category_name: str, campaign_name: str, weight_update: WeightUpdate):
+    data = get_clients_data(client_id)
+
+    if source_name not in data \
+        or "categories" not in data[source_name] \
+            or category_name not in data[source_name]["categories"] \
+                or "campaigns" not in data[source_name]["categories"][category_name] \
+                    or campaign_name not in data[source_name]["categories"][category_name]["campaigns"]:
         raise HTTPException(status_code=404, detail="Campaign not found")
 
-    category = Category(**data[source_name]["categories"][category_name])
-    campaign = Campaign(**category.campaigns[campaign_name])
+    current_campaign = data[source_name]["categories"][category_name]["campaigns"][campaign_name]
+    current_category = data[source_name]["categories"][category_name]
 
-    total_campaign_weight = sum(c.w for c in category.campaigns.values()) - campaign.w + weight_update.w
+    total_campaign_weight = sum(campaign["w"] for campaign in current_category["campaigns"].values()) \
+                            - current_campaign["w"] + weight_update.w
+
     if total_campaign_weight != 100:
-        raise HTTPException(status_code=400, detail="Total campaign weight must equal 100 within the category")
+        raise HTTPException( status_code=400, detail="Total campaign weight must equal 100 within the category")
 
-    campaign.w = weight_update.w
-    data[source_name]["categories"][category_name]["campaigns"][campaign_name] = campaign.dict()
-    return data[source_name]["categories"][category_name]["campaigns"][campaign_name]
+    current_campaign["w"] = weight_update.w
+    return current_campaign
